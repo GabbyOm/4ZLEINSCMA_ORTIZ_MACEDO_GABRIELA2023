0fed5ff4603add06aa6fd7dee103f05d794e530c
GabbyOm <g.ortiz.m680@edu.utc.mx>
2023-09-23 20:32:35 -0600

    Menu de Estructuras

---
+++ 03EstructurasControl/Estructurasdecontrol.java
@@ -0,0 +1,32 @@
+/*
+  * vamos a crear un programa mediante el cual debebamos tener un meno
+  de seleccion para elegir alguna de las 14 opciones siguientes:
+  1- Bono o descuento por edad
+  2- Convertir numeros decimales  binarios
+  3- Convertir temperaturas
+  4- Numeros de positivos o negativos
+  5- tiendita
+  6- Area o perimetro
+  7- Tabla
+  8- Factorial
+  9- Dibujitos
+  10- Figura basica
+  11- Patron
+  12- Diamante
+  13- Calculadora
+  14- Salir
+
+  */
+//declarar son las librerias que vamos a ocupar 
+//se debe estructutrar el tipo de datos acorde a su entrada
+//si es entero obtenerlo como entero, si char obtenerlo como caracter
+import java.util.Scanner;
+
+class EstructurasControl{
+
+     public static void main(String[] args){
+     
+
+     }
+
+}
