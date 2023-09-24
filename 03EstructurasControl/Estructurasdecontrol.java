f443ffe839df0c9cb8683893d8b00afee7792fc2
GabbyOm <g.ortiz.m680@edu.utc.mx>
2023-09-23 20:41:11 -0600

    Estructuras Control

--- 03EstructurasControl/Estructurasdecontrol.java
+++ 03EstructurasControl/Estructurasdecontrol.java
@@ -23,10 +23,53 @@
 import java.util.Scanner;
 
 class EstructurasControl{
-
+//si declaramos aqui las variables son globales
      public static void main(String[] args){
-     
+     //el manejo de objeto, nos ayuda a poder instancear,(mandando a llamar al objeto)
+      //para hacer una instancia, es necesario 1- indentificar el tipo de objeto
+      //2-nombrar al objeto
+      //3-Crear al objeto
+        Scanner entrada = new Scanner(System.in);
+       //crear una instancia del objeto de la entrada por defecto a la computadora
+      //vamos a identificar con Scanner el tipo de dato que se esta instanceando 
+      //entrada es el objeto que va apoder identificar si es int,double,flot,etc
 
+      //es declarar a las variables que se van a utilizar en el programa 
+      //las variables son de dos tipos globales y locales
+      //si la declaro dentro del metodo es local
+       
+
+      int opcion, numbinario, total, cantidadproducto, num1;
+      float precio, resultado = 0, compra = 0;
+      String nombreproducto;
+      String binario = "";
+      char letra;
+
+      
+      do{
+           //aqui va todo el programa 
+            System.out.println ("Bienvenido al programa:3");
+            System.out.println ("Elije alguna opcion deseada");
+            System.out.println ("1- Descuento por edad ");
+            System.out.println ("2- Covertir nuemro decimal a binario");//jimmy
+            System.out.println ("3- Covertir Temperaturas");
+            System.out.println ("4- numeros positivos y negativos ");
+            System.out.println ("5- tiendita");//jimmy
+            System.out.println ("6- Area y Perimetro");
+            System.out.println ("7- tabla");//jimmy
+            System.out.println ("8- Factorial");
+            System.out.println ("9- Dibujito");//jimmy
+            System.out.println ("10- Figura Hueca");
+            System.out.println ("11- Patrones de Codigo");
+            System.out.println ("12- Diamante ");
+            System.out.println ("13- calculadora");
+            System.out.println ("14- Salir");    
+          
+
+        System.out.println("Desea repetir el programa?, escribe s para si");
+        letra = entrada.next().charAt(0);
+
+      }while(letra == 's' || letra == 'S');
      }
 
 }
